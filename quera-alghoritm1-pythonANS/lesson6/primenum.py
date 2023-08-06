27ed35c0367ec2e0a7c57132c96a62e2ac153c2c
arianFR <arian2012f@gmail.com>
2023-08-07 03:26:04 +0330

    new code

--- quera-alghoritm1-pythonANS/lesson6/primenum.py
+++ quera-alghoritm1-pythonANS/lesson6/primenum.py
@@ -4,13 +4,14 @@ def prime_num(a, b):
     for i in range(a, b + 1):
         is_prime = True
         
-        for j in range(2, int(math.sqrt(i)) + 1):
+        for j in range(2, int(math.sqrt(i) + 1)):
             if i % j == 0:
                 is_prime = False
                 break
         
         if is_prime:
-            print(i)
+            if i != 1:
+                print(i)
 
 num1 = int(input())
 num2 = int(input())
